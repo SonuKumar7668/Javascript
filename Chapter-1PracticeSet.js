76f5861fa9c06c1cdf7465d69025f96851de76dc
Sonu <sonu108rp@gmail.com>
2023-10-07 20:23:08 +0530

    chapter 1 excersize

---
+++ Chapter-1PracticeSet.js
@@ -0,0 +1,35 @@
+//Chapter 1
+//Q - 1
+let a = "sonu"
+let b = 34
+console.log(a + b)
+
+//Q - 2
+
+console.log(typeof a)
+
+//Q - 3
+
+const d ={
+    name : "harry",
+    section: 1,
+    principle: false,
+}
+console.log(d ["name"])
+// d = 54
+
+//Q - 4
+d['friend'] = "wintr"
+console.log(d ["friend"])
+
+//Q - 5
+
+const dict = {
+    appreciate:"recognize the full worth of.",
+    subway:"a tunnel under a road for use by pedestrians.",
+    King:"the male ruler of an independent state, especially one who inherits the position by right of birth.",
+    aole:"not at all.",
+    horripilation:"goosebumps.",
+    nepobaby:"a celebrity with a parent who is also famous, especially one whose industry connections are perceived as essential to their success.",
+}
+console.log(dict.King)
