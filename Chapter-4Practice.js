e499f6fa09e0d5e80e3b4e076ef3fdb04f3e102b
Sonu <sonu108rp@gmail.com>
2023-10-15 20:42:34 +0530

    Chapter 4 complete

--- Chapter-4Practice.js
+++ Chapter-4Practice.js
@@ -2,3 +2,21 @@
 let str = "sonu\""
 console.log(str)
 console.log(str.length)
+const sentence = "the quick brown fox jumps over a lazy dog"
+const word = "fox"
+console.log(`the word ${word} ${sentence.includes(word)? 'is': 'is not'} included in the sentence `);
+// Q No 2
+console.log(sentence.includes(word))
+let text = "Hello world, welcome to the universe.";
+console.log(text.startsWith("Hello"));
+// Q No 3
+console.log(text.endsWith("universe."))
+console.log(sentence.toLowerCase())
+console.log(sentence.toUpperCase())
+// Q No 4
+let str1 = "Please give me RS 1000"
+let amount = Number.parseInt(str1.slice("Please give me RS ".length))
+console.log(amount)
+// Q No 5
+let friend = "Deepika "
+console.log(friend.replace("p","r"))
