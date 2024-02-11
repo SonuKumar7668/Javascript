977b59492bcf420d04796669cf3abeafe96b7fe2
Sonu <sonu108rp@gmail.com>
2024-02-11 22:11:42 +0530

    marge

---
+++ condition.js
@@ -0,0 +1,10 @@
+let age = 15;
+let grace = 4;
+if(age+grace<18){
+    console.log("not adult");
+}
+else{
+    console.log("adult");
+}
+console.log(age ** grace);
+console.log(age % grace);
---
+++ script.js
@@ -0,0 +1,34 @@
+// alert("hello There");
+console.log("hello world");
+var a = 12;
+var b = 11;
+var c = "sonu";
+console.log(a+b);
+console.log(b+8);
+console.log(typeof a, typeof b);
+console.log(typeof b);
+console.log(typeof c);
+const a1 = 6;
+// a1 = a1+1; not allowed because a1 is constant.
+let d = 5;
+{
+    let d = 6;
+    console.log(d);
+}
+console.log(d);
+let x = "sonu";
+let y = 22;
+let z = 3.34;
+let p = true;
+let q = undefined;
+let r = null;
+console.log(x,y,z,p,q,r);
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
+let o={
+    "name": "salery",
+    "job code":5600,
+    "is_handsome": true
+}
+console.log(o);
+o.salery = 20;
+console.log(o);
