98e5dfe919f8f941a8bed7bc9f446c0a3e276392
Sonu <sonu108rp@gmail.com>
2024-09-22 22:58:45 +0530

    till API

---
+++ API/collage.js
@@ -0,0 +1,32 @@
+let url="http://universities.hipolabs.com/search?name=";
+let btn=document.querySelector("button");
+btn.addEventListener("click", async ()=>{
+    console.log("button was clicked");
+    let country=document.querySelector("input").value;
+    console.log(country);
+    let colleges= await getreq(country);
+    console.log(colleges);
+    show(colleges);
+});
+
+async function show(colleges){
+    let list=document.querySelector(".list");
+    list.innerText="";
+    for(col of colleges){
+        console.log(col.name);
+        let li=document.createElement("li");
+        li.innerText=col.name
+        list.appendChild(li);
+    }
+}
+// let country="India";
+async function getreq(country){
+    try{
+        let res=await axios.get(url+country);
+        console.log(res);
+        return res.data;
+    }catch(err){
+        console.log("Error",err);
+    }
+}
+getreq();
--- API/script.js
+++ API/script.js
@@ -1,14 +1,31 @@
 let url="https://catfact.ninja/fact";
-let fct;
-fetch(url)
-.then((response)=>{
-    console.log(response);
-    return response.json();
-})
-.then((data)=>{
-    console.log(data.fact);
-    document.querySelector("h1").innerText=data.fact;
-})
-.catch((err)=>{
-    console.log("Error: ",err);
+let url2="https://dog.ceo/api/breeds/image/random"
+let btn=document.querySelector("button");
+async function getfact(){
+    try{
+    let ft=await axios.get(url);
+    console.log(ft.data.fact);
+    // document.querySelector("#fact").innerText=ft.data.fact;
+    return(ft.data.fact);
+    }
+    catch(e){
+        console.log("err",e);
+    }
+}
+
+async function getimg(){
+    try{
+        let mg=await axios.get(url2);
+        console.log(mg.data.message);
+        return mg.data.message;
+    }
+    catch(e){
+        console.log("Error",e);
+    }
+}
+btn.addEventListener("click",async ()=>{
+    let facts=await getfact();
+    document.querySelector("#fact").innerText=facts;
+    let imgs=await getimg();
+    document.querySelector("img").setAttribute("src",imgs)
 });
