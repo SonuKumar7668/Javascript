1a7ca305213f067e4f4c770b6d2bde1d59df5ee7
Sonu <sonu108rp@gmail.com>
2024-09-30 22:45:52 +0530

    npm install

---
+++ Node/process.js
@@ -0,0 +1,7 @@
+// let arg=process.argv;
+// for(let i=0;i<arg.length;i++){
+//     console.log("hello i am ",arg[i]);
+// }
+// console.log(process.argv);
+let x=require("./sum");
+console.log(x);
---
+++ Node/sum.js
@@ -0,0 +1,9 @@
+const sum=(a,b)=>a+b;
+const mul=(a,b)=>a*b;
+const sub=(a,b)=>a-b;
+let obj={
+    sum:sum,
+    mul:mul,
+    sub:sub
+}
+module.exports= obj;
